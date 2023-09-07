dd67c463dc76769bcf7c8f88b05f8e6815cb36b7
Chachi20 <santanahormigaencarna@gmail.com>
2023-09-07 23:58:46 +0100

    avanzando

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
 
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
+
+import './Home.css'
+
+function Home() {
+    return (
+        <>
+            <header className="header">
+                <a href="#" className="logo">Encarna S.H.</a>
+
+                <nav className="navbar">
+                    <a href="#">Home</a>
+                    <a href="#">Porfolios</a>
+                    <a href="#">Contactos</a>
+
+                </nav>
+            </header>
+
+            <section className="home">
+                <div className="contenido">
+                    <h1>Hola, Soy Encarnación Santana</h1>
+                    <h3>Front Developer</h3>
+                    <p>Me gusta la programación, desde el diseño de web, app, etc. Decí tomar este camino, porque me gusta, ya que no tiene que ver mis estudios realizados.</p>
+                </div>
+
+                <div className="sci">
+                    <a href="#"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAX1JREFUSEvtlU8rRFEYxn/vFZqNe8bGSjHKLETJyp+y8AlkzkSyVspC2VjZKyXlG2jSDF9BFlJkLU3yb6WUGbNggXs0M3eumblSHLLg1qn3nNt9fs/7nHs6wg8/8sP6hAE7+RhOs8d45PI74LWATG4dZN4XXkWrRVvIGyB9G0caT2sEHdPBRPTKBlIFKMQRrxbwbDqZilpFVRdRfgOYKzuWNbS7YOO+pBIS2Cp04xiPpHtmKx4GZApD8NJUbsB5JOEelurtXD8Gt1Qb54Gke0T6vhW8EZBekAYg9Z6p+ohugDbf+TladZXqdG4fkeEymCxGlsGk6hLwgCW0Wqnu/PMAuAMi/ginaBgjqXYrL74CqHxbBF0DfYATkIQUCTVtCTCb6OiMH98oIntvrZgTdLTHDvDixJlsyQai2/lrDO3+/B6tlB3gzm1iVp4CQCZ/DAwEc62C6D/agwu0ioX+ouJCwnUQMVWAA2DwH1A+aH88ovDBt175hUvf2nOtwCvT7rgZzIqmjgAAAABJRU5ErkJggg=="/></a>
+                    <a href="#"> <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAftJREFUSEvdlT9oE2EYxn/vl2gDLcldVVBQ/AMiYi0iTjoqgiKo7d2hFDroINJdUZS66+JQcZFQCYjGdBCEDl1EuhQFRQcniwUxKDE5pODfe+VSahpzvSaBLP3Ge9/v+fG8791zQoePdFif1Qa4X+xmeON87NhypTSp5CjoCZTtwC+EMdZlrlL8uolE8iRuegwRDXVqI3qkCcR/iwYeXu+bZSH5yhRwOKI+B2xBOYtnPVys1wAPyttIyizwBWNOM5iebhApVPYT8DLG4QvgGao5PPtVvYOspujxvwHJqoBwD1lzncHuT/8E8/4QaC4GMI/oCI493uggfJIvT4McrBeQ56BPMDqDmn2o3o4BZHGtc0vrS3bwuQfW7kKYBNa39X0IN3GsS9GAQmkzQeIDUAI2tAVARnAzd6IBN9Swx38PbG1PPNyb2Y2TfhcNWNjBAEj4ii0surUzi2vt+P9KY1Q8rngo14C9LekLF3Gsu00A/OOoPm1JHObQzE48+bkyoDoqfxx0uEnIb1QO4WVmovqj0zRceF+5DzVXgDMxIAU9j2tnl+tpBISBl+o6gMhl4FiM+HfQIVx7Is5pDVCoHOEPtxD660Iw+vYkGlzA6w0DLvbUO6hGsXEIzClEjwJdwA+gCPoRYQrDBAP265WEF+ur7Y/WrO8W+jo+or9WFIoZB2AA2wAAAABJRU5ErkJggg=="/></a>
+               
+                </div>
+            </section>
+        </>
+    );
 }
 
 export default Home;
