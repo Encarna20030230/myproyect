b614c2bac94f019ffab750250fe30754e9714c84
Chachi20 <santanahormigaencarna@gmail.com>
2023-09-10 20:59:02 +0100

    Terminado el porfolio, hay que corregirlo

--- src/pages/Porfolio.jsx
+++ src/pages/Porfolio.jsx
@@ -1,13 +1,32 @@
+import './Porfolio.css'
+
 const Porfolios = () => {
     return (
         <>
-        <h1>
-            HASTA LOS COJONES
-            esta es la de porfolio
-        </h1>
+            <h1>
+                FORMULARIO DE PROYECTO
+            </h1>
+            <form action="procesar_formulario.php" method="post">
+                <label htmlFor="nombre" >Nombre del Proyecto:</label>
+                <input type="text" id="nombre" name="nombre" required></input>
 
+                <label htmlFor="descripción">Descripción:</label>
+                <textarea id="descripción" name="descripción" required></textarea>
+
+                <label htmlFor="imagen">Imagen del Proyecto:</label>
+                <input type="file" id="imagen" name="imagen" required></input>
+
+                <label htmlFor="repositorio">Enlace de Repositorio:</label>
+                <input type="url"id="repositorio"name="repositorio"required></input>
+
+                <label htmlFor="despliegue">Enlace de Despliegue:</label>
+                <input type="url"id="despliegue"name="despliegue"required></input>
+
+                <input type="submit"value="Enviar"></input>
+            </form>
+            
         </>
     )
-} 
+}
 
 export default Porfolios;
