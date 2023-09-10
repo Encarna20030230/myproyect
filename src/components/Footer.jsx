f91bdbc94ce655e3f4fae511c023322a51391a9f
Chachi20 <santanahormigaencarna@gmail.com>
2023-09-10 20:03:50 +0100

    Terminado la parte de contact

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import './footer.css';
+
+function Footer() {
+    return (
+        <>
+            <a href="#" className="fa fa-twitter"></a>
+
+            <a href="#" className="fa fa-linkedin"></a>
+        </>
+    )
+}
+
+export default Footer;
