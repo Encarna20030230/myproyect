c958f7a491e0367a4936edccdc35df981aef4306
Chachi20 <santanahormigaencarna@gmail.com>
2023-09-10 12:57:36 +0100

    video en formato mp4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
+import ReactPlayer from "react-player";
+
 const Home = () => {
     return (
         <>
-        <h1>
-            HASTA LOS COJONES
-            ESTO ES UNA MIERDA 
-            ANIMO
-        </h1>
+        <section className="home">
+                <div className="contenido">
+                    <h1>Hola, Soy Encarnación Santana</h1>
+                    <h3>Front Developer</h3>
+                    <p>Me gusta la programación, desde el diseño de web, app, etc. Decí tomar este camino, porque me gusta, ya que no tiene que ver mis estudios realizados.</p>
+                </div>
+                <div className='Video'>
+                    <ReactPlayer url="src/components/Pegote.mp4" controls="true" playing="true" width="150%" />
+                </div>
+
+            </section>
 
         </>
     )
